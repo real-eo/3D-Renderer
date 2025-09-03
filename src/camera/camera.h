80fc06548567701067bfb7a3dfae590a1cad9155
Elias Alexander Wiklund Ottersbo <realeawo5@gmail.com>
2025-09-03 04:33:39 +0200

    game class

--- src/camera/camera.h
+++ src/camera/camera.h
@@ -9,8 +9,8 @@ private:
 
 public:
     Vec3 pos;
-    float yaw = 0.0f;   // rotation around y-axis
-    float pitch = 0.0f; // rotation around x-axis
+    float yaw;      // rotation around y-axis
+    float pitch;    // rotation around x-axis
 
     Camera() : pos{0, 0, -5}, yaw{0}, pitch{0} {}
     Camera(const Vec3& position, float yawAngle, float pitchAngle) : pos(position), yaw(yawAngle), pitch(pitchAngle) {}
